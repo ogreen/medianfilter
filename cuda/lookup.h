65527e20dc84ebe90a8a684694ecd142c8bfaa42
Oded Green <ogreen@gatech.edu>
2015-07-11 22:59:51 -0400

    Moving older histogram computational functions

--- cuda/lookup.h
+++ cuda/lookup.h
@@ -236,3 +236,205 @@ __device__ void histogramMedianPar128(hist_type* H,hist_type* Hscan,const int32_
 	}
 	syncthreads();
 }
+
+ 
+__device__ void histogramMedianPar64(hist_type* H,hist_type* Hscan,const int32_t size, const int32_t medPos, const int logSize,im_type* retval){
+	int32_t tx=threadIdx.x;
+	*retval=1;
+
+	Hscan[tx]=H[tx];
+	Hscan[tx+64]=H[tx+64];
+	Hscan[tx+128]=H[tx+128];
+	Hscan[tx+192]=H[tx+192];	
+	syncthreads();
+
+	int32_t add;
+
+//	if(tx<128){
+	add=tx<<1; Hscan[add+1]+=Hscan[add];
+	add=64+tx<<1; Hscan[add+1]+=Hscan[add];
+
+	syncthreads();
+	if(tx<64){
+		add=tx<<2; Hscan[add+3]+=Hscan[add+1];
+	}
+	syncthreads();
+
+	if(tx<32){
+		add=tx<<3;Hscan[add+7]+=Hscan[add+3];
+	}
+	if(tx<16){
+		add=tx<<4; Hscan[add+15]+=Hscan[add+7];
+	}
+	if(tx<8){
+		add=tx<<5; Hscan[add+31]+=Hscan[add+15];
+	}
+	if(tx<4){
+		add=tx<<6; Hscan[add+63]+=Hscan[add+31];
+	}
+	if(tx<2){
+		add=tx<<7; Hscan[add+127]+=Hscan[add+63];
+	}
+	Hscan[MF_HIST_SIZE-1]=0;
+	syncthreads();
+
+	hist_type temp;
+	if (tx<1){
+		add=tx<<8;
+		temp=Hscan[add+127]; Hscan[add+127]=Hscan[add+255]; Hscan[add+255]+=temp;
+	}
+	if (tx<2){
+		add=tx<<7;
+		temp=Hscan[add+63]; Hscan[add+63]=Hscan[add+127]; Hscan[add+127]+=temp;
+	}
+	if (tx<4){
+		add=tx<<6;
+		temp=Hscan[add+31]; Hscan[add+31]=Hscan[add+63]; Hscan[add+63]+=temp;
+	}
+	if (tx<8){
+		add=tx<<5;
+		temp=Hscan[add+15]; Hscan[add+15]=Hscan[add+31]; Hscan[add+31]+=temp;
+	}
+	if (tx<16){
+		add=tx<<4;
+		temp=Hscan[add+7]; Hscan[add+7]=Hscan[add+15]; Hscan[add+15]+=temp;
+	}
+	if (tx<32){
+		add=tx<<3;
+		temp=Hscan[add+3]; Hscan[add+3]=Hscan[add+7]; Hscan[add+7]+=temp;
+	}
+	syncthreads();
+	if (tx<64){
+		add=tx<<2;
+		temp=Hscan[add+1]; Hscan[add+1]=Hscan[add+3]; Hscan[add+3]+=temp;
+	}
+	syncthreads();
+//	if (tx<128){
+	{
+		add=tx<<1;
+		temp=Hscan[add+0]; Hscan[add+0]=Hscan[add+1]; Hscan[add+1]+=temp;
+		
+		add=64+tx<<1;
+		temp=Hscan[add+0]; Hscan[add+0]=Hscan[add+1]; Hscan[add+1]+=temp;
+		
+	}
+	syncthreads();
+	tx=threadIdx.x;
+	for(; tx<255;tx+=blockDim.x){
+		if(Hscan[tx+1]>=medPos){
+			if(Hscan[tx]<medPos){ 
+				*retval=tx;
+			}
+			break;
+			
+		}
+	}
+	syncthreads();
+}
+
+
+__device__ void histogramMedianPar32Multi(hist_type* H,hist_type* Hscan,const int32_t size, const int32_t medPos, const int logSize,im_type* retval){
+	int32_t tx=threadIdx.x;
+	*retval=0;
+	__shared__ int32_t foundIn;
+	foundIn=31;
+//	if(tx<32)
+//	{
+		Hscan[tx]=0;
+		int add=tx<<3;
+		for(int i=0; i<8;i++){
+			Hscan[tx]+=H[add+i];
+		}
+
+		if(tx<16){
+			add=tx<<1; Hscan[add+1]+=Hscan[add];
+		}
+		if(tx<8){
+			add=tx<<2; Hscan[add+3]+=Hscan[add+1];
+		}
+		if(tx<4){
+			add=tx<<3; Hscan[add+7]+=Hscan[add+3];
+		}
+		if(tx<2){
+			add=tx<<4; Hscan[add+15]+=Hscan[add+7];
+		}
+
+		Hscan[31]=0;
+		
+		hist_type temp;
+		if (tx<1){
+			add=0;
+			temp=Hscan[add+15]; Hscan[add+15]=Hscan[add+31]; Hscan[add+31]+=temp;
+		}
+		if (tx<2){
+			add=tx<<4;
+			temp=Hscan[add+7]; Hscan[add+7]=Hscan[add+15]; Hscan[add+15]+=temp;
+		}
+		if (tx<4){
+			add=tx<<3;
+			temp=Hscan[add+3]; Hscan[add+3]=Hscan[add+7]; Hscan[add+7]+=temp;
+		}
+		if (tx<8){
+			add=tx<<2;
+			temp=Hscan[add+1]; Hscan[add+1]=Hscan[add+3]; Hscan[add+3]+=temp;
+		}
+		if (tx<16){
+			add=tx<<1;
+			temp=Hscan[add]; Hscan[add]=Hscan[add+1]; Hscan[add+1]+=temp;
+		}
+	
+		syncthreads();
+		if(tx<31){
+			if(Hscan[tx+1]>=medPos){
+				if(Hscan[tx]<medPos){ 
+					foundIn=tx;
+				}
+			}
+		}	
+//			if(blockIdx.x==0 && threadIdx.x==0){
+//				printf("%d  %d  %d %d \n",Hscan[foundIn], Hscan[foundIn+1], foundIn,medPos);
+//			}
+
+		
+		syncthreads();
+		if(tx==0){
+			int32_t pos=foundIn<<3;
+			int32_t total=Hscan[foundIn]+H[pos];
+			for(int i=0; i<8;i++)
+			{
+				if(total>=medPos){
+					*retval=pos+i;
+					break;
+				}
+				total+=H[pos+i+1];
+			}
+//			if(blockIdx.x==0){
+//				for(int i=0; i<32; i++)
+//					printf("%d ", Hscan[i]);
+//				printf("      -  %d  %d  %d  %d\n",foundIn, foundIn<<3,Hscan[foundIn], total);
+//			}
+			
+		}
+//	}
+
+
+
+	/*
+	syncthreads();
+	tx=threadIdx.x;
+	for(; tx<255;tx+=blockDim.x){
+		if(Hscan[tx+1]>=medPos){
+			if(Hscan[tx]<medPos){ 
+				*retval=tx;
+			}
+			break;
+			
+		}
+	}
+	syncthreads();
+*/	
+}
+
+_
+
+
